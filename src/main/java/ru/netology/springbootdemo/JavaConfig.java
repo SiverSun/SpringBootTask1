a080b564f56d7763ee358a4d688824f8ccae8731
Svetlana Tokarchuk <lanawm73@gmail.com>
2023-01-06 15:30:28 -0400

    Config changed

--- src/main/java/ru/netology/springbootdemo/JavaConfig.java
+++ src/main/java/ru/netology/springbootdemo/JavaConfig.java
@@ -1,5 +1,6 @@
 package ru.netology.springbootdemo;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,13 +8,13 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class JavaConfig {
     @Bean
-    @Profile("dev")
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    @Profile("prod")
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
